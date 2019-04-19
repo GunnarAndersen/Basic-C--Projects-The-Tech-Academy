49bb8d11317aeb62abd88e6b26a8a2830c245891
Gunnar Andersen <45769452+GunnarAndersen@users.noreply.github.com>
2019-04-19 17:46:40 -0600

    Method Exercise
    Creating a class and then making an object, then creating methods to compliment the class.

---
+++ MethodsExercise/Numbers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise
+{
+    public class Numbers
+    {
+        public Numbers()
+        {
+            Value = 7;
+        }
+        public int Value { get; set; }
+
+        public static int addTwoInts(int n) //Was having issues. I needed to make this so that it is a static int
+        {
+            Console.WriteLine("Adding Numbers...");
+            int result = n + 2;
+            Console.WriteLine(result);
+            return result;
+        }
+
+        public static int subTwoInts(int n)
+        {
+            Console.WriteLine("Subtracting Numbers...");
+            int result = n - 2;
+            Console.WriteLine(result);
+            return result;
+        }
+        public static int multTwoInts(int n)
+        {
+            Console.WriteLine("Multiplying Numbers...");
+            int result = n * 2;
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+        
+       
+}
+    
+
---
+++ MethodsExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers uNum = new Numbers();
+            Console.WriteLine("Write down a number. I am going to some experiments on it, but it won't die so don't worry.");
+            uNum.Value = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(uNum.Value);
+            Numbers.addTwoInts(uNum.Value); //takes an int argument so I put in uNum.Value which is an int
+            Numbers.subTwoInts(uNum.Value);
+            Numbers.multTwoInts(uNum.Value);
+            Console.ReadLine();
+        }
+    }
+}
