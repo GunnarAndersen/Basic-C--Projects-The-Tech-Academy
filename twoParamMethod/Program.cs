f4a4406d393035d4c8d6be26b44a4c0a7ecce37c
Gunnar Andersen <45769452+GunnarAndersen@users.noreply.github.com>
2019-04-19 22:40:16 -0600

    Two Parameter Method
    Making a method that takes in two parameters while printing the unaffected parameter.

---
+++ twoParamMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twoParamMethod
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            nums uNum = new nums();
+            Console.WriteLine("Hi, please pick a number");
+            int choice1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Okay now I need another number");
+            int choice2 = Convert.ToInt32(Console.ReadLine());
+            nums.exercise(choice1, choice2);
+        }
+
+     
+    }
+}
---
+++ twoParamMethod/nums.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twoParamMethod
+{
+    public class nums
+    {
+        public nums()
+        {
+            worth = 7;
+        }
+        public int worth { get; set; }
+
+        public static void exercise(int n, int m)
+        {
+            
+            int one = n;
+            int two = m;
+            int newOne = one + 5;
+            Console.WriteLine("is "+ two+" your second number?");
+            Console.ReadLine();
+
+        }
+    }
+}
