b99d1a10bfdf74c7a319471d51487b227f094567
Gunnar Andersen <45769452+GunnarAndersen@users.noreply.github.com>
2019-04-19 23:21:41 -0600

    Overloaded Methods
    In this exercise I made several methods with the same name, but they take different parameters. From there, I was able to instruct the computer in such a way that the compiler would use the correct method every time.

---
+++ OverloadedMethods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadedMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            numbers stuff = new numbers();
+            numbers.mathFunction(12);
+            numbers.mathFunction(3.14m);
+            numbers.mathFunction("5");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ OverloadedMethods/numbers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadedMethods
+{
+    class numbers
+    {
+        public numbers()
+        {
+            Value = 7;
+        }
+        public int Value { get; set; }
+
+        public static int mathFunction(int n) //Was having issues. I needed to make this so that it is a static int
+        {
+            Console.WriteLine("Adding Numbers...");
+            int result = n + 2;
+            Console.WriteLine(result);
+            return result;
+        }
+
+        public static int mathFunction(decimal n) //second version of this
+        {
+            Console.WriteLine("Modulo!");
+            int result = Convert.ToInt32(n % 2);
+            Console.WriteLine(result);
+            return result;
+        }
+
+        public static int mathFunction(string n) //s version of this
+        {
+            Console.WriteLine("Whoa there! let's make that string into an integer if possible....");
+            int integer = Convert.ToInt32(n);
+            Console.WriteLine("Let's make it bigger!");
+            int result = integer * 6;
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+}
