c721cd8fee3325e02d76ff350d1e133bf22b51cf
Gunnar Andersen <45769452+GunnarAndersen@users.noreply.github.com>
2019-04-20 16:52:19 -0600

    Add files via upload

---
+++ Inheritance/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Employee : Person //this makes the employee class inherit from the person class.
+    {
+        public Employee()
+        {
+            Id = 1;
+        }
+        public int Id { get; set; }
+    }
+}
---
+++ Inheritance/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Person
+    {
+        public Person()
+        {
+            FirstName = "Gunnar";
+            LastName = "Andersen";
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName(Employee sample)
+        {
+            Console.WriteLine("Name:"+ FirstName +" "+ LastName);
+        }
+    }
+}
