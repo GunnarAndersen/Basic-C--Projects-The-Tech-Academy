f6c4e80aeb54f70fd86c602373584f3a49a8d640
Gunnar Andersen <45769452+GunnarAndersen@users.noreply.github.com>
2019-04-20 14:27:40 -0600

    More method related work
    Making a static class and overloading a method

---
+++ moreMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moreMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write down an integer");
+            int uNum = Convert.ToInt32(Console.ReadLine());
+            numbers.mathFunction(uNum);
+            int uNum2 = 6;
+            numbers.mathFunction(out uNum, out uNum2);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ moreMethods/numbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moreMethods
+{
+    static class numbers
+    {
+        
+        public static void mathFunction(int m)
+        {
+            Console.WriteLine("dividing your number...");
+            Console.WriteLine(m / 2);
+        }
+        public static void mathFunction(out int x, out int y)
+        {
+            Console.WriteLine("Hi, please give me a new integer");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(x);
+            Console.WriteLine("Now adding your integer");
+            y = x + 2;
+            Console.WriteLine(y);
+
+        
+    }   }
+}
