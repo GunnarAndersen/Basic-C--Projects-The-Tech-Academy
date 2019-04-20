773c6a6d4825d28ee2cfab1bfbb7c8b67001cad2
Gunnar Andersen <45769452+GunnarAndersen@users.noreply.github.com>
2019-04-19 23:59:27 -0600

    Making use of optional parameters
    Using optional parameters and then ensuring that the program still functions correctly.

---
+++ OptionalParams/OptionalParams/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParams
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            numbers leest = new numbers();
+            Console.WriteLine("Please give me one number at a time.The second is optional");
+            int hang = Convert.ToInt32(Console.ReadLine());
+            numbers.mathFunction(hang);
+            
+        }
+    }
+}
---
+++ OptionalParams/OptionalParams/numbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParams
+{
+    class numbers
+    {
+        public static void mathFunction(int req, int opt = 0) //need to have an optional parameter
+        { 
+            try
+            {
+                
+                Console.WriteLine("Another, please... but you don't have to. It is optional after all");
+                opt = Convert.ToInt32(Console.ReadLine());
+                int result = req + 5 + opt;
+                Console.WriteLine(result);
+                
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Sorry, we're just going with the default now.");
+                opt = 0;
+                Console.WriteLine(req + 5 + opt);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
