bbb4a6cca20b4fc0682422f097477947f98087f7
Gunnar Andersen <45769452+GunnarAndersen@users.noreply.github.com>
2019-04-20 22:50:15 -0600

    Using abstract classes
    Inheriting a method from an abstract class and then utilizing it

---
+++ Abstract/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    public abstract class Person
+    {
+        public Person()
+
+        {
+            string firstName = "Gunnar";
+            string lastName = "Andersen";
+        }
+
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine("Name:" + firstName + " " + lastName);
+        }
+    }
+
+   
+}
---
+++ Abstract/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Sample = new Employee {firstName = "Sample", lastName ="Student" };
+            Sample.SayName();
+            Console.ReadLine();
+        }
+    }
+}
