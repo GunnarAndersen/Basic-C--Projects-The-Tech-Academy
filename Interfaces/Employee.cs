dbfeafab7535cd44c065f0a5aad8cee858d46222
Gunnar Andersen <45769452+GunnarAndersen@users.noreply.github.com>
2019-04-21 11:17:29 -0600

    Interfaces
    Using interfaces and polymorphism

---
+++ Interfaces/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Employee : Person, IQuittable
+    {
+         //this makes the employee class inherit from the person class.
+        
+            public Employee()
+            {
+                Id = 1;
+            }
+            public int Id { get; set; }
+
+            public void Quit(Employee employee)
+        {
+            Console.WriteLine("I have HAD IT WITH THIS JOB! I'M DONE!");
+            Console.ReadLine();
+        }
+        
+    }
+}
