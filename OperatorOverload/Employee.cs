1a234a3ed9eab707c91d442b4895b46ad286d19b
Gunnar Andersen <45769452+GunnarAndersen@users.noreply.github.com>
2019-04-21 16:05:51 -0600

    Overloading Operators
    Overloaded == and != Operators

---
+++ OperatorOverload/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Employee : Person, IQuittable
+    {
+         //this makes the employee class inherit from the person class.
+        
+            public List<Employee> Employees { get; set; }
+            public Employee()
+            {
+                Id = 1;
+            }
+            public int Id { get; set; }
+
+            public void Quit(Employee employee)
+        {
+            Console.WriteLine("I have HAD IT WITH THIS JOB! I'M DONE!");
+            Console.ReadLine();
+        }
+
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            if (employee1.Id == employee2.Id)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            if (employee1.Id != employee2.Id)
+                return true;
+            else
+                return false;
+        }
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode(); 
+        }
+
+
+    }
+}
---
+++ OperatorOverload/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee() { Id = 1, FirstName = "Gunnar", LastName = "Andersen" };
+            Employee employee2 = new Employee() { Id = 2, FirstName = "Katie", LastName = "Wise" };
+            Employee employee3 = new Employee() { Id = 3, FirstName = "Andrew", LastName = "Smith" };
+            Employee employee4 = new Employee() { Id = 1, FirstName = "Lionel", LastName = "Busque" };
+            if (employee1 == employee2)
+                Console.WriteLine("equal");
+            else Console.WriteLine("not equal");
+
+            if (employee2 == employee2)
+                Console.WriteLine("equal");
+            else Console.WriteLine("not equal");
+
+            if (employee1 == employee4)
+                Console.WriteLine("equal");
+            else Console.WriteLine("not equal");
+
+            Console.ReadLine();
+        }
+    }
+}
