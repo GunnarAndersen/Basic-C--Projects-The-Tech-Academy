5e6944e3bd4487ac2f90a2a3403fe491a2fb48af
Gunnar Andersen <45769452+GunnarAndersen@users.noreply.github.com>
2019-04-21 22:02:27 -0600

    Using Generics
    Generic classes

---
+++ Generics/Generics/Interfaces.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable employee1 = new Employee() { FirstName = "Gunnar", LastName = "Andersen", Id = 1 };
+            Employee employee2 = new Employee() { FirstName = "Timothy", LastName = "Tugmutton", Id = 2 };
+            employee1.Quit(employee2);
+        }
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employeeNames = new Employee<string>() {Things = new List<string>() { "Bob", "Tom", "Ralph", "Stephen", "Stanley" } };
+            Employee<int> employeeId = new Employee<int>() { Things = new List<int>() { 1, 2, 3, 4, 5 } };
+
+            //these lists need to be put in this fashion or else it will give a conversion error. 
+
+            foreach (string employee in employeeNames.Things)
+                {
+                Console.WriteLine(employee);
+                }
+            //we print "employee" because if we do not it will not cycle through the object list and give us the names.
+          
+            foreach (int Id in employeeId.Things)
+            {
+                Console.WriteLine(Id);
+            }
+            Console.ReadLine();
+            
+
+        }
+    }
+
+}
