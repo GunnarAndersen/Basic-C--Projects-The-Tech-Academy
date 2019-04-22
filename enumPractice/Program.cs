d966c3961bd50087b1ae8935f66198a46e82369d
Gunnar Andersen <45769452+GunnarAndersen@users.noreply.github.com>
2019-04-22 10:25:10 -0600

    eNums practice
    Using eNum and exception handling.

---
+++ enumPractice/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumPractice
+{
+    class Program
+    {
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hey, what day of the week is it?");
+                DaysOfTheWeek currentDay = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), Console.ReadLine());
+                Console.WriteLine(currentDay);
+            }
+            catch (ArgumentException ex) // figured out that I should use this because when I tried to compile it said "argumentexception"
+            {
+                Console.WriteLine("Error, please enter an actual day of the week");
+                
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
