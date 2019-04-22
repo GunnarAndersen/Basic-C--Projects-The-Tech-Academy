eaa41c725dfdd023d4f3834d992ec1fc40ccb8d3
Gunnar Andersen <45769452+GunnarAndersen@users.noreply.github.com>
2019-04-22 11:21:12 -0600

    Structs
    Using structs to assign property values

---
+++ enumPractice/Structs/Structs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number Price = new Number();
+            Price.Amount = 3.14m;
+            Console.WriteLine(Price.Amount);
+            Console.ReadLine();
+        }
+    }
+}
