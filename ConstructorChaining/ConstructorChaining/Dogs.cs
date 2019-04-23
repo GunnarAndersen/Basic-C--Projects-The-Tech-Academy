7f115f4924ddd36cef93e893935e30fbee7b8694
Gunnar Andersen <45769452+GunnarAndersen@users.noreply.github.com>
2019-04-23 17:05:54 -0600

    Constructor Chaining
    Using constructor chaining to quickly and easily instantiate my dogs

---
+++ ConstructorChaining/ConstructorChaining/Dogs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChaining
+{
+    public class Dog
+    {
+        public string Name;
+        public int Age;
+
+        public Dog() : this("", 0)
+        {
+        }
+        public Dog(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+            
+        }
+        public Dog(int Age) : this("", Age)
+        {
+        }
+        public Dog(string Name) : this(Name, 0)
+        {
+        }
+        
+
+    }
+}
---
+++ ConstructorChaining/ConstructorChaining/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChaining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string dog1Name = "Amy";
+            var BlackLab = new Dog(dog1Name, 7);
+
+            const string dog2Name = "Henri";
+            var Beagle = new Dog(dog2Name, 2);
+
+            
+        }
+    }
+}
