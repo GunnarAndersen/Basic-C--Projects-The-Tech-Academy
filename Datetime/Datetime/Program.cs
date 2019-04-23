12371ad63065a7966ae22429a16ab9d70ce0aed4
Gunnar Andersen <45769452+GunnarAndersen@users.noreply.github.com>
2019-04-23 11:57:39 -0600

    Datetime
    Using Datetime and other methods to see what time it will be in x hours

---
+++ Datetime/Datetime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentYear = DateTime.Now;
+            Console.WriteLine(currentYear);
+            Console.WriteLine("Hi, please give me a number to look at.");
+            int x = Convert.ToInt32(Console.ReadLine());
+            DateTime futureTime = currentYear.AddHours(x);
+            Console.WriteLine("In " + x + " hours it will be...");
+            Console.WriteLine(futureTime);
+            Console.ReadLine();
+        }
+    }
+}
