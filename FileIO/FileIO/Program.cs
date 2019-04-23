a209b1e21aefc6b82438559cf0a6fa0b7015d5c7
Gunnar Andersen <45769452+GunnarAndersen@users.noreply.github.com>
2019-04-23 11:14:10 -0600

    Using System.IO
    FileIO methods

---
+++ FileIO/FileIO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, please give me a number.");
+            int uNum = int.Parse(Console.ReadLine());
+            string rlyANum = Convert.ToString(uNum);
+            File.WriteAllText(@"C:\nums\nums.txt", rlyANum);
+
+            Console.WriteLine(rlyANum);
+            Console.ReadLine();
+        }
+    }
+}
