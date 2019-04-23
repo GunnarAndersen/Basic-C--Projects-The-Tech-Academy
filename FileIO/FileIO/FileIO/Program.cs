de8d42a5434d02e02488569ea1ec089af4f7cfda
Gunnar Andersen <45769452+GunnarAndersen@users.noreply.github.com>
2019-04-23 13:10:00 -0600

    Using Systems.IO
    writing a txt file and printing the results back to the user

---
+++ FileIO/FileIO/FileIO/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, please give me a number.");
+            int uNum = int.Parse(Console.ReadLine());
+            string rlyANum = Convert.ToString(uNum);
+            File.WriteAllText(@"C:\nums\nums.txt", rlyANum);
+            
+            string txtContents = System.IO.File.ReadAllText(@"C:\nums\nums.txt");
+
+            // Display the file contents to the console. Variable text is a string.
+            System.Console.WriteLine("Contents of your text file = {0}", txtContents);
+            Console.ReadLine();
+        }
+    }
+}
